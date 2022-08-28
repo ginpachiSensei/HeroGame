db310c9339cca567725ce74ef0707692fa720ab5
ginpachiSensei <satyamrocker1234@gmail.com>
2022-08-28 05:34:22 +0530

    added gui class and added logo to the window

--- source/HeroGame.cpp
+++ source/HeroGame.cpp
@@ -4,10 +4,14 @@ CHeroGame *CHeroGame::s_instance = NULL;
 
 CHeroGame::CHeroGame() : CGame("HeroGame", {1280, 720})
 {
-    const std::string texture_dir = HERO_RES_PATH + "Textures/";
+    const std::string textures_dir = HERO_RES_PATH + "Textures/";
+
+    for (auto texture : {"Logo"})
+        textureManager().loadFromFile(texture, textures_dir + texture + ".png");
+
     for (std::string texture : {"sky"})
     {
-        textureManager().loadFromFile(texture, texture_dir + "Background/" + texture + ".png");
+        textureManager().loadFromFile(texture, textures_dir + "Backgrounds/" + texture + ".png");
         textureManager().get(texture)->setRepeated(true);
     }
 }
@@ -21,4 +25,25 @@ CHeroGame *CHeroGame::instance()
     return s_instance;
 }
 
-void CHeroGame::init() {}
+void CHeroGame::init()
+{
+    getRootObject()->addObject(m_gui_object = new CHeroGUI());
+}
+
+CHeroGame &HeroGame()
+{
+    return *CHeroGame::instance();
+}
+
+CHeroGUI::CHeroGUI()
+{
+    const int y_gui_pos = 5;
+    m_game_logo = new CLabel(sf::Sprite(*CHeroGame::instance()->textureManager().get("Logo"), {0, 0, 750, 300}));
+    m_game_logo->setPosition(280, 70);
+    addObject(m_game_logo);
+}
+
+void CHeroGUI::draw(sf::RenderWindow *render_window)
+{
+    CGameObject::draw(render_window);
+}
--- source/HeroGame.h
+++ source/HeroGame.h
@@ -4,14 +4,31 @@
 
 const std::string HERO_RES_PATH = "res/";
 
+class CHeroGUI;
+
 class CHeroGame : public CGame
 {
 private:
     CHeroGame();
     static CHeroGame *s_instance;
     virtual void init() override;
+    CHeroGUI *m_gui_object = NULL;
 
 public:
     static CHeroGame *instance();
     ~CHeroGame();
+};
+
+CHeroGame &HeroGame();
+
+class CHeroGUI : public CGameObject
+{
+public:
+    CHeroGUI();
+
+protected:
+    void draw(sf::RenderWindow *render_window) override;
+
+private:
+    CLabel *m_game_logo;
 };
