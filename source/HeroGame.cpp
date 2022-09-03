240427e60c0baecf39dfe849e5baf167ce3f8ead
ginpachiSensei <satyamrocker1234@gmail.com>
2022-09-03 23:27:52 +0530

    added scene set and stage set function in HeroGame class

--- source/HeroGame.cpp
+++ source/HeroGame.cpp
@@ -25,9 +25,42 @@ CHeroGame *CHeroGame::instance()
     return s_instance;
 }
 
+void CHeroGame::setScene(CGameObject *new_scene)
+{
+    m_current_scene = new_scene;
+    new_scene->turnOff();
+    getRootObject()->addObject(new_scene);
+    m_scene_stack.push_back(new_scene);
+    m_level_name = new_scene->getName();
+}
+
+void CHeroGame::setState(GameState state)
+{
+    m_game_state = state;
+    switch (state)
+    {
+        case(GameState::main_menu){
+            loadLevel(m_first_level);
+            m_gui_object->setState(GUIState::menu);
+            m_current_scene->enable();
+            m_current_scene->update(0);
+            m_current_scene->disable();
+            break;
+        }
+    }
+}
+
 void CHeroGame::init()
 {
     getRootObject()->addObject(m_gui_object = new CHeroGUI());
+    setState(GameState::main_menu);
+}
+
+void CHeroGame::loadLevel(const std::string &_level_name)
+{
+    std::string level_name = _level_name;
+    // create new scene and set it to the current scene
+    m_current_stage_name = level_name;
 }
 
 //----------------------------------------------------------------------------------------
--- source/HeroGame.h
+++ source/HeroGame.h
@@ -17,9 +17,29 @@ private:
     static CHeroGame *s_instance;
     virtual void init() override;
     CHeroGUI *m_gui_object = NULL;
+    enum class GameState
+    {
+        main_menu
+    } m_game_state = GameState::main_menu;
+    std::vector<CGameObject *> m_scene_stack;
+    void loadLevel(const std::string &level_name);
+    CGameObject *m_current_scene = NULL;
+    std::string m_level_name;
+    std::string m_first_level;
+    void setScene(CGameObject *new_scene);
+    void setState(GameState state);
+    std::string m_current_stage_name;
 };
 
 CHeroGame &HeroGame();
+
+enum class GUIState
+{
+    normal,
+    status,
+    menu,
+    gameover
+};
 
 class CHeroGUI : public CGameObject
 {
